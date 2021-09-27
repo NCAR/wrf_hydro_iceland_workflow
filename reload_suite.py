8ceef1bd0d3ebfad0bb3551ff20970ec56fdff19
Andrew Gaydos <gaydos@cheyenne1.cheyenne.ucar.edu>
2021-09-27 14:58:49 -0600

    Added manual pages.

--- reload_suite.py
+++ reload_suite.py
@@ -3,10 +3,13 @@ from ecflow import Client
 try:
     print("Reloading suite...")
     ci = Client()
-    ci.delete_all(force=True)           # clear out the server
+    try:
+        ci.delete("/wrf_hydro_iceland", force=True)           # clear out the server
+    except:
+        pass
     ci.load("defs/iceland.def")       # load the definition into the server
     
-    #ci.begin_suite("wrf_hydro_test")    # start the suite
+    #ci.begin_suite("wrf_hydro_iceland")    # start the suite
     ci.suspend("wrf_hydro_iceland")
     print("Complete")
 except RuntimeError as e:
